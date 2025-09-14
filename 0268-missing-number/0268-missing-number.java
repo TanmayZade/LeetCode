4fe1f5574225a7b784c9c8c02c2190c20536da8b
Tanmay Zade <tanmayzade87@gmail.com>
2025-09-14 11:53:29 +0530

    Time: 0 ms (100%), Space: 45.4 MB (56.64%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int freq[] = new int[n + 2];
-        for(int i = 0;i < n;i++) freq[nums[i]]++;
-        for(int i = 0;i < n+2;i++) if(freq[i] == 0) return i;
-        return 0;
+        // int freq[] = new int[n + 2];
+        // for(int i = 0;i < n;i++) freq[nums[i]]++;
+        // for(int i = 0;i < n+2;i++) if(freq[i] == 0) return i;
+        // return 0;
+        int sum = n*(n+1)/2;
+
+        int currSum = 0;
+
+        for(int i = 0;i < n;i++) currSum += nums[i];
+
+        return (sum - currSum);
     }
 }
