2328e43b70c3944c3f9cf73fe58b81fc2594f48c
Tanmay Zade <tanmayzade87@gmail.com>
2025-09-22 22:27:27 +0530

    Edited file via Streamlit

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,30 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-
-        for(int i = 0;i < nums.length;i++){
-            for(int j = i+1; j < nums.length;j++){
-                if(nums[i] + nums[j] == target){
-                    result[0] = i;
-                    result[1] = j;
-                    break;
-                }
-            }
-        }
-        return result;
-    }
-}
+1  imprt os
+2  def my_function(x y)
+3      prin("Hello" + x + y
+4  
+5  API_KEY = "sk-rand0mFAKEapiKEY123456789xyz"
+6  
+7  clas Example
+8      def _init_(self, name)
+9          self.name = names
+10 
+11     def greet(self)
+12         print "Hello" self.name
+13 
+14 numbers = [1, 2, 3, 
+15 for n in numbers
+16     printn
+17 
+18 if True
+19 print("Always true")
+20 
+21 dic = {"a":1, "b",2 "c":3}
+22 
+23 whle i < 5:
+24     print(i)
+25     i + 1
+26 
+27 def broken_func(
+28     return "unreachable"
+29 
+30 prin("Done")
