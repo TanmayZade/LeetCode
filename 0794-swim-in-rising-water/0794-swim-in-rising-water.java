be247b423b7aaa8bb60de1730aa342fd8ffe72fc
Tanmay Zade <tanmayzade87@gmail.com>
2025-10-06 12:35:11 +0530

    Time: 10 ms (77.4%), Space: 44.5 MB (48.94%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -1,33 +1,30 @@
-import java.util.*;
-
 class Solution {
     public int swimInWater(int[][] grid) {
         int n = grid.length;
-        boolean[][] visited = new boolean[n][n];
-        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-        
-        // Min-heap based on elevation
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-        pq.offer(new int[]{0, 0, grid[0][0]});
-        visited[0][0] = true;
-        
+
+        boolean[][] vis = new boolean[n][n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[2] - b[2]));
         int maxTime = 0;
-        
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int x = cur[0], y = cur[1], height = cur[2];
-            maxTime = Math.max(maxTime, height);
-            
-            if (x == n - 1 && y == n - 1) return maxTime;
-            
-            for (int[] d : dirs) {
-                int nx = x + d[0], ny = y + d[1];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n && !visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    pq.offer(new int[]{nx, ny, grid[nx][ny]});
+        pq.offer(new int[]{0,0,grid[0][0]});
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,1,-1};
+
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int x = curr[0],y = curr[1], ht = curr[2];
+            maxTime = Math.max(maxTime, ht);
+
+            if(x == n-1 && y == n-1) return maxTime;
+
+            for(int i = 0;i < 4;i++){
+                int newX = x + dx[i], newY = y + dy[i];
+                if(newX < n && newY < n && newX >= 0 && newY >= 0 && !vis[newX][newY]){
+                    vis[newX][newY] = true;
+                    pq.offer(new int[]{newX,newY, grid[newX][newY]});
                 }
             }
         }
         return maxTime;
+
     }
 }
