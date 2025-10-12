b335140007dd15b50970a44f1b967f660ed67c42
Tanmay Zade <tanmayzade87@gmail.com>
2025-10-12 12:49:40 +0530

    Time: 8 ms (45.2%), Space: 40.4 MB (66.23%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,21 +1,7 @@
-import java.util.*;
-
 class Solution {
-    HashMap<Integer, Integer> hm  = new HashMap<>();
     public int fib(int n) {
         if(n <= 1) return n;
-        int lastDigit = n-1;
-        if(!hm.containsKey(n-1)){
-            int lastDSum = fib(n-1);
-            hm.put(lastDigit, lastDSum);
-        }
-            int lastSecondDigit = n-2;        
-        if(!hm.containsKey(lastSecondDigit)){
-            int lastSSecondDigit = fib(n-2);
-            hm.put(lastSecondDigit, lastSSecondDigit);
-        }
-        return hm.get(lastDigit) + hm.get(lastSecondDigit);
+
+        return fib(n-1) + fib(n-2);
     }
-
-
 }
