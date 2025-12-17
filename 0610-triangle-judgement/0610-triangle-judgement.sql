0534e7393e5b8d13bb8e0e20a692804e03f6d812
Tanmay Zade <tanmayzade87@gmail.com>
2025-12-17 19:19:04 +0530

    Time: 304 ms (18.52%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,3 +1,2 @@
 # Write your MySQL query st
-SELECT *, IF((x+y > z) AND (y+z > x) AND (z+x > y),'Yes','No') triangle
-FROM triangle;
+select *, if((x+y > z) and (z+x>y) and (y+z>x) ,"Yes","No")as triangle from Triangle
