f01118c56d9e915988a90fe2085727bd29854994
Tanmay Zade <tanmayzade87@gmail.com>
2025-12-18 20:22:48 +0530

    Time: 514 ms (57.74%), Space: 0B (100%) - LeetHub

--- 3642-find-books-with-polarized-opinions/3642-find-books-with-polarized-opinions.sql
+++ 3642-find-books-with-polarized-opinions/3642-find-books-with-polarized-opinions.sql
@@ -1,37 +1,15 @@
-SELECT
-    b.book_id,
-    b.title,
-    b.author,
-    b.genre,
-    b.pages,
-    MAX(r.session_rating) - MIN(r.session_rating) AS rating_spread,
-    ROUND(SUM(
-        CASE 
-            WHEN r.session_rating <= 2 OR r.session_rating >= 4 THEN 1
-            ELSE 0
+# Write your MySQL query statement below
+SELECT b.book_id, title, author, genre, pages, MAX(session_rating)-MIN(session_rating) rating_spread,
+    ROUND((SUM(
+        (CASE 
+        WHEN r.session_rating <= 2 OR r.session_rating >= 4
+        THEN 1
+        ELSE 0
         END
-    ) * 1.0 / COUNT(*),2) AS polarization_score
+        )
+    ) * 1.0 / COUNT(*) ), 2)AS polarization_score
 FROM books b
-JOIN reading_sessions r
-    ON b.book_id = r.book_id
-GROUP BY
-    b.book_id,
-    b.title,
-    b.author,
-    b.genre,
-    b.pages
-HAVING
-    COUNT(*) >= 5
-    AND MAX(r.session_rating) >= 4
-    AND MIN(r.session_rating) <= 2
-    -- AND (
-    --     SUM(
-    --         CASE 
-    --             WHEN r.session_rating <= 2 OR r.session_rating >= 4 THEN 1
-    --             ELSE 0
-    --         END
-    --     ) * 1.0 / COUNT(*)
-    -- ) >= 0.6
-ORDER BY
-    polarization_score DESC,
-    b.title DESC;
+JOIN reading_sessions r USING(book_id)
+GROUP BY book_id,title, author, genre, pages
+HAVING COUNT(*) >= 5 AND MAX(r.session_rating) >= 4 AND MIN(r.session_rating) <= 2
+ORDER BY polarization_score DESC, TITLE DESC
